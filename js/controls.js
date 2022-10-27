f21d8f7913f151c5226a5206332bdacf66cbc1f7
PedroNnsM <pedronunes95@outlook.com>
2022-10-27 08:29:06 -0300

    projeto ok

--- js/controls.js
+++ js/controls.js
@@ -1,42 +1,42 @@
 export default function Controls({
-    buttonPause,
-    buttonPlay,
-    buttonSet,
-    buttonStop
-}){
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop
+}) {
 
-    function play(){
-        buttonPlay.classList.toggle('hide')
-        buttonPause.classList.toggle('hide')
-        buttonStop.classList.remove('hide')
-        buttonSet.classList.add('hide')
+  function play() {
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.remove('hide')
+    buttonSet.classList.add('hide')
+    buttonStop.classList.remove('hide')
+  }
+
+  function pause() {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+  }
+  
+  function reset() {
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    buttonSet.classList.remove('hide')
+    buttonStop.classList.add('hide')
+  }
+
+  function getMinutes() {
+    let newMinutes = prompt('Quantos minutos?')
+    if (!newMinutes) {
+      return false
     }
 
-    function pause(){
-        buttonPlay.classList.toggle('hide')
-        buttonPause.classList.toggle('hide')
-    }
-    
-   
-    function reset()  {
-        buttonPlay.classList.remove('hide')
-        buttonPause.classList.add('hide')
-        buttonStop.classList.add('hide')
-        buttonSet.classList.remove('hide') 
-    }
+    return newMinutes
+  }
 
-    function getMinutes(){
-        let newMinutes = prompt('Quantos minutos ?')
-        if (!newMinutes) {
-            return false
-        }  
-    }
-        
-    return {
-        reset,
-        play,
-        pause,
-        getMinutes
-        
-    }
+  return {
+    reset,
+    play,
+    pause,
+    getMinutes
+  }
 }
--- js/index.js
+++ js/index.js
@@ -1,67 +1,29 @@
-import Controls from './controls.js'
-import  Timer  from './timer.js'
-
-const buttonPlay = document.querySelector('.play')
-const buttonPause = document.querySelector('.pause')
-const buttonStop = document.querySelector('.stop')
-const buttonSet = document.querySelector('.set')
-const buttonSoundOn = document.querySelector('.sound-on')
-const buttonSoundOff = document.querySelector('.sound-off')
-
-const minutesDisplay = document.querySelector('.minutes')
-const secondsDisplay = document.querySelector('.seconds')
-let minutes = Number(minutesDisplay.textContent)
+import Controls from "./controls.js"
+import Timer from "./timer.js"
+import Sound from "./sounds.js"
+import Events from "./events.js"
+import {
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop,
+  minutesDisplay,
+  secondsDisplay,
+} from "./elements.js"
 
 const controls = Controls({
-    buttonPause,
-    buttonPlay,
-    buttonSet,
-    buttonStop,
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop
 })
 
 const timer = Timer({
-    minutesDisplay,
-    secondsDisplay,
-    resetControls: controls.reset,
-    minutes
+  minutesDisplay, 
+  secondsDisplay, 
+  resetControls: controls.reset
 })
 
-buttonPlay.addEventListener('click', () => {
-    controls.play()
-    timer.countdown()
-});
+const sound = Sound()
 
-buttonPause.addEventListener('click', () => {
-    controls.pause()
-    timer.hold()
-});
-
-buttonStop.addEventListener('click', ()=> {
-    
-    controls.reset()
-    timer.reset()
-});
-
-buttonSoundOn.addEventListener('click', () => {
-    buttonSoundOn.classList.add('hide')
-    buttonSoundOff.classList.remove('hide')
-} )
-
-buttonSoundOff.addEventListener('click', () => {
-    buttonSoundOff.classList.add('hide')
-    buttonSoundOn.classList.remove('hide')
-} )
-
-buttonSet.addEventListener( 'click', () => {
-    let newMinutes = controls.getMinutes()
-
-    if (!newMinutes){
-        timer.reset()
-        return
-    }
-
-    minutes = newMinutes
-    timer.updateDisplay(minutes, 0)
-    timer.updateMinutes(minutes)
-})
-
+Events({controls, timer, sound})
